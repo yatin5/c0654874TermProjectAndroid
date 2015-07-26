b57bbc7a50e472a956b2fc35c4ecbc80a887a58a
yatin <yatin_3313@hotmail.com>
2015-07-26 18:21:08 -0400

    Implemented code foe winner check or tie and if there is no more moves.

--- TermProject/src/com/example/termproject/TacTacToeGame.java
+++ TermProject/src/com/example/termproject/TacTacToeGame.java
@@ -4,12 +4,10 @@ import java.util.Random;
 
 public class TacTacToeGame {
 
-	
 	private char gameBoard[];
-	
-	
+
 	private final static int boardSize = 9;
-	
+
 	public static int getBoardsize() {
 		return boardSize;
 	}
@@ -18,44 +16,96 @@ public class TacTacToeGame {
 	public static final char cpu = '0';
 	public static final char emptySpace = ' ';
 	private Random gameRand;
-	
 
-	public TacTacToeGame(){
-		
+	public TacTacToeGame() {
+
 		gameBoard = new char[boardSize];
-		 for(int i=0; i < boardSize; i++)
-			 gameBoard[i] = emptySpace;
-		 gameRand = new Random();
+		for (int i = 0; i < boardSize; i++)
+			gameBoard[i] = emptySpace;
+		gameRand = new Random();
 	}
-	
-	public void cleanBoard(){
-		
-		for(int i=0; i<boardSize; i++){
-			
+
+	public void cleanBoard() {
+
+		for (int i = 0; i < boardSize; i++) {
+
 			gameBoard[i] = emptySpace;
 		}
 	}
-	
-	public void setMove(char player, int location){
-		
+
+	public void setMove(char player, int location) {
+
 		gameBoard[location] = player;
 	}
+
+	public int getBoardMove() {
+
+		int move;
+		for (int i = 0; i < getBoardsize(); i++) {
+			if (gameBoard[i] != user && gameBoard[i] != cpu) {
+				{
+					char current = gameBoard[i];
+					gameBoard[i] = cpu;
+
+					if (checkForWinner() == 3) {
+						setMove(cpu, i);
+						return i;
+					} else
+						gameBoard[i] = current;
+				}
+			}
+		}
+		for (int i = 0; i < getBoardsize(); i++) {
+			if (gameBoard[i] != user && gameBoard[i] != cpu) {
+				{
+					char current = gameBoard[i];
+					gameBoard[i] = user;
+
+					if (checkForWinner() == 2) {
+						setMove(cpu, i);
+						return i;
+					} else
+						gameBoard[i] = current;
+				}
+			}
+		}
+		do
+		{
+			move = gameRand.nextInt(getBoardsize());
+		}while(gameBoard[move] == user || gameBoard[move] == cpu);
+		
+		setMove(cpu, move);
+	return move;
+	}
 	
-//	public int getBoardMove(){
-//		
-//		int move;
-//		for (int i = 0; i < getBoardsize(); i++) {
-//			if(gameBoard[i] != user && gameBoard[i]!= cpu){
-//				{
-//					char current = gameBoard[i];
-//					gameBoard[i] = cpu;
-//					
-//					if(checkForWinner() == 3){
-//						setMove(cpu,i);
-//					}
-//				}
-//			}
-//			
-//		}	
-//	}
+	public int checkForWinner() {
+		
+		for(int i = 0; i <= 6; i += 3){
+			if(gameBoard[i] == user && gameBoard[i+1] == user && gameBoard[i+2] == user ){
+				return 2;}
+			if(gameBoard[i] == cpu && gameBoard[i+1] == cpu && gameBoard[i+2] == cpu ){
+				return 3;}
+		}
+		for(int i = 0; i <= 2; i ++ ){
+			if(gameBoard[i] == user && gameBoard[i+3] == user && gameBoard[i+6] == user ){
+				return 2;}
+			if(gameBoard[i] == cpu && gameBoard[i+3] == cpu && gameBoard[i+6] == cpu ){
+				return 3;}
+		}
+		if(gameBoard[0] == user && gameBoard[4] == user && gameBoard[8] == user || gameBoard[2] == user && gameBoard[4] == user && gameBoard[6] == user )
+		{
+			return 2;
+		}
+		if(gameBoard[0] == cpu && gameBoard[4] == cpu && gameBoard[8] == cpu || gameBoard[2] == cpu && gameBoard[4] == cpu && gameBoard[6] == cpu )
+		{
+			return 3;
+		}
+		for(int i = 0; i < getBoardsize(); 	i++){
+			if(gameBoard[i] != user && gameBoard[i] != cpu){
+				return 0;
+			}
+		}
+		return 1;
+	}
+
 }
