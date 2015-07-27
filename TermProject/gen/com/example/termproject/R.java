6bdcab07916c7ba2fb718f8571baa19bf290648a
yatin <yatin_3313@hotmail.com>
2015-07-26 23:49:25 -0400

    main activity clas

--- TermProject/gen/com/example/termproject/R.java
+++ TermProject/gen/com/example/termproject/R.java
@@ -25,7 +25,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int Game=0x7f080000;
+        public static final int GameBoard=0x7f080001;
+        public static final int Information=0x7f08000e;
+        public static final int Tie=0x7f080010;
+        public static final int action_settings=0x7f080016;
+        public static final int cpu=0x7f080012;
+        public static final int cpuTotal=0x7f080013;
+        public static final int eight=0x7f08000c;
+        public static final int five=0x7f080008;
+        public static final int four=0x7f080007;
+        public static final int nine=0x7f08000d;
+        public static final int one=0x7f080003;
+        public static final int seven=0x7f08000b;
+        public static final int six=0x7f080009;
+        public static final int tableRow1=0x7f080002;
+        public static final int tableRow2=0x7f080006;
+        public static final int tableRow3=0x7f08000a;
+        public static final int tableRow4=0x7f08000f;
+        public static final int three=0x7f080005;
+        public static final int tieCount=0x7f080011;
+        public static final int two=0x7f080004;
+        public static final int user=0x7f080014;
+        public static final int userTotal=0x7f080015;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,9 +56,28 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int Cpu=0x7f050002;
+        public static final int User=0x7f050001;
+        public static final int action_settings=0x7f050015;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int cpu=0x7f050012;
+        public static final int cpu_wins=0x7f050007;
+        public static final int eight=0x7f05000f;
+        public static final int five=0x7f05000c;
+        public static final int four=0x7f05000b;
+        public static final int info=0x7f050011;
+        public static final int nine=0x7f050010;
+        public static final int one=0x7f050008;
+        public static final int seven=0x7f05000e;
+        public static final int six=0x7f05000d;
+        public static final int three=0x7f05000a;
+        public static final int tie=0x7f050005;
+        public static final int ties=0x7f050014;
+        public static final int turn_cpu=0x7f050004;
+        public static final int turn_user=0x7f050003;
+        public static final int two=0x7f050009;
+        public static final int user=0x7f050013;
+        public static final int user_wins=0x7f050006;
     }
     public static final class style {
         /** 
--- TermProject/src/com/example/termproject/MainActivity.java
+++ TermProject/src/com/example/termproject/MainActivity.java
@@ -1,16 +1,99 @@
 package com.example.termproject;
 
+import android.R.color;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter.ViewBinder;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+//import com.example.termproject.R;
 
 public class MainActivity extends Activity {
+	
+	private TacTacToeGame Game;
+	private Button gameBoardButtons[];
+	private TextView infoTextView;
+	private TextView tieCount;
+	private TextView userTotal;
+	 private TextView cpuTotal;
+	private int userIncerement = 0;
+	private int cpuIncerement = 0;
+	private int tieIncerement = 0;
+	
+	private boolean userFirst = true;
+	private boolean gameOver = false;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	
+	gameBoardButtons = new Button[TacTacToeGame.getBoardsize()];
+	gameBoardButtons[0] = (Button) findViewById(R.id.one);
+	gameBoardButtons[1] = (Button)findViewById(R.id.two);
+	gameBoardButtons[2] = (Button)findViewById(R.id.three);
+	gameBoardButtons[3] = (Button)findViewById(R.id.four);
+	gameBoardButtons[4] = (Button)findViewById(R.id.five);
+	gameBoardButtons[5] = (Button)findViewById(R.id.six);
+	gameBoardButtons[6] = (Button)findViewById(R.id.seven);
+	gameBoardButtons[7] = (Button)findViewById(R.id.eight);
+	gameBoardButtons[8] = (Button)findViewById(R.id.nine);
+	
+	 infoTextView = (TextView) findViewById(R.id.Information);
+	 userTotal = (TextView) findViewById(R.id.userTotal);
+	 tieCount = (TextView) findViewById(R.id.tieCount);
+	 cpuTotal = (TextView) findViewById(R.id.cpuTotal);	
+	 
+	 userTotal.setText(Integer.toString(userIncerement));
+	 tieCount.setText(Integer.toString(tieIncerement));
+	 cpuTotal.setText(Integer.toString(cpuIncerement));
+	 
+	 Game = new TacTacToeGame();
+	 
+	 startGame();
+	 	
+}
+	private void startGame()
+	{
+		Game.cleanBoard();
+		
+		for(int i = 0; i < gameBoardButtons.length; i++)
+		{
+			gameBoardButtons[i].setText("");
+			gameBoardButtons[i].setEnabled(true);
+			gameBoardButtons[i].setOnClickListener(new ButtonClickListener(i));
+		}
+		if(userFirst)
+		{
+			infoTextView.setText(R.string.User);
+			userFirst = false;
+		}
+		else
+		{
+			infoTextView.setText(R.string.turn_cpu);
+			int move = Game.getBoardMove();
+			setMove(Game.cpu, move);
+			userFirst = true;
+		}
+	}
+	private void setMove(char player, int location)
+	{
+		Game.setMove(player, location);
+		gameBoardButtons[location].setEnabled(false);
+		gameBoardButtons[location].setText(String.valueOf(player));
+		if(player == Game.user)
+		{
+			gameBoardButtons[location].setTextColor(Color.GREEN);
+			
+		}
+		else {
+			gameBoardButtons[location].setTextColor(Color.RED);
+		}
 	}
 
 	@Override
@@ -30,5 +113,47 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private class ButtonClickListener implements View.OnClickListener {
+		int location;
+		public ButtonClickListener(int location) {
+			this.location = location;
+		}
+		public void onClick(View view) {
+			if(gameOver){
+				if(gameBoardButtons[location].isEnabled()){
+					setMove(Game.user, location);
+					int winner = Game.checkForWinner();
+					if(winner == 0){
+						infoTextView.setText(R.string.turn_cpu);
+						int move = Game.getBoardMove();
+						setMove(Game.cpu, move);
+						winner = Game.checkForWinner();
+					}
+					else if (winner == 0) {
+						infoTextView.setText(R.string.turn_user);
+					}
+					else if(winner == 1) {
+						infoTextView.setText(R.string.tie);
+						tieIncerement++;
+						tieCount.setText(Integer.toString(tieIncerement));
+						gameOver = true;
+					}
+					else if(winner == 2) {
+						infoTextView.setText(R.string.user_wins);
+						userIncerement++;
+						userTotal.setText(Integer.toString(userIncerement));
+						gameOver = true;
+					}
+					else {
+						infoTextView.setText(R.string.cpu_wins);
+						cpuIncerement++;
+						tieCount.setText(Integer.toString(tieIncerement));
+						gameOver = true;
+					}
+				}
+			}
+		}
 	}
 }
